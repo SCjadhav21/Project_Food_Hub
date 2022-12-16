5ca2677818c83422acd55745e0e3ba50890dc4e9
AnjuTewatia <anjutewatia008@gmail.com>
2022-12-16 22:30:55 +0530

    day-4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
- 
+ <ChakraProvider>
+ <BrowserRouter>
 
       <App />
+ </BrowserRouter>
+ </ChakraProvider>
+
  
 );
 
