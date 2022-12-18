b579a275caf5de68300b420d69c92b6e461918e1
AnjuTewatia <anjutewatia008@gmail.com>
2022-12-18 11:33:13 +0530

    final commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components copy/Home/Home';
-import SigleUserPage from './components copy/Home/Meal/SigleUserPage';
-import FoodMenu from './components copy/Home/Meal/FoodMenu';
-import { Route, Routes } from 'react-router-dom';
-import RestaurantSingle from './components copy/Home/Banner/RestaurantSingle/RestaurantSingle';
+import React from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
-     <Home/>
-     {/* <FoodMenu /> */}
-     
-     <Routes>
-      <Route path="/compo" element={<SigleUserPage/>} />
-     <Route path="/rest" element={<RestaurantSingle />} />
-     </Routes>
+      <Navbar />
     </div>
-  )
+  );
 }
 
 export default App;
